3dfbcc586bda1df589902f3123ea2008ff4bd7dc
mmbehiclayton <30318074+mmbehiclayton@users.noreply.github.com>
2021-07-09 19:45:32 +0300

    Add files via upload

---
+++ concat.py
@@ -0,0 +1,16 @@
+"""
+String Concatenation
+To concatenate, or combine, two strings you can use the + operator.
+
+Example
+Merge variable a with variable b into variable c:
+"""
+a="Clayton"
+b="Mmbehi"
+#concat the two variables
+
+#try
+c=a+b
+print(c)
+
+print(a+ " " +b)
