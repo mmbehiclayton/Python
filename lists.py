69b92f628b97207d279b7321e2409d940539a5d5
Clayton Mmbehi <30318074+mmbehiclayton@users.noreply.github.com>
2021-07-10 13:38:31 +0300

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,13 @@
+mylist=list(("book", "pen","rubber","pencil"))
+
+#print list
+print(mylist)
+#Print the second item of the list: i.e. pen
+print(mylist[1])
+
+#change list values
+mylist[1]=["log book"]
+print(mylist)
+
+mylist[0]=["Mathematical Set"]
+print(mylist)
