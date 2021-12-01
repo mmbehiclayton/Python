43db28238614410587b29b34465eab6da6499c3b
Clayton Mmbehi <30318074+mmbehiclayton@users.noreply.github.com>
2021-12-01 11:17:39 +0300

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,13 @@
+#there are three numeric types in python
+#int, float and complex
+a=1 #int
+b=2.8 #float
+c=7j #complex
+d="Clayton" #string
+e=1.22 #float
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
