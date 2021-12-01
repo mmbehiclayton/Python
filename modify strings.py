b0ec0112136f45917fe094736f65781149124350
Clayton Mmbehi <30318074+mmbehiclayton@users.noreply.github.com>
2021-12-01 11:35:50 +0300

    Add files via upload

---
+++ modify strings.py
@@ -0,0 +1,16 @@
+a="Clayton"
+b="JOHN"
+
+print(a.upper()) #changes to upper case
+print(b.lower()) #changes b to lower case
+
+#remove white space
+x=" Hello Clay "
+print(x.strip())
+
+#Replace String
+print(x.replace("Hello","Hi"))
+
+#Split String
+y=x.split(",")
+print(y)
